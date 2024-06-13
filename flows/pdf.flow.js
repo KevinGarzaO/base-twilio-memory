b1d9033ae7746cb7e690e853779274b2445a438b
Kevin Garza <kevin_4569@msn.com>
2024-06-12 21:52:23 -0600

    se agrego imagen y se aplazo tiempo
    se agrego imagen y se aplazo tiempo

--- flows/pdf.flow.js
+++ flows/pdf.flow.js
@@ -1,7 +1,7 @@
 const { addKeyword,  EVENTS } = require("@bot-whatsapp/bot");
 
 module.exports = addKeyword(EVENTS.ACTION)
-.addAction({delay: 300000}, async (ctx, {flowDynamic}) =>{
+.addAction({delay: 600000}, async (ctx, {flowDynamic}) =>{
     await flowDynamic(
       "Hola de nuevo, soy Caroline, me comunico para dar seguimiento y unirme a tu equipo de ventas" +
       "\n Necesitare un *PDF* con la información de tu negocio:"+
@@ -10,6 +10,7 @@ module.exports = addKeyword(EVENTS.ACTION)
       "\n 3.- Las objeciones de porque no comprpan tu producto o servicio"+
       "\n 4. Algunas ofertas activas"+
       "\n 5.- Cualquier informacion adicional para que yo puedo entednder de tu negocio es de mucha ayuda para mi"+
-      "\n\n Puedes regresar conmigo cuando tengas este *PDF*, escribiendo la palabra *PDF*"
-     );
+      "\n\n Puedes regresar conmigo cuando tengas este *PDF*, escribiendo la palabra *PDF*",
+      { media: "https://babelink.com.mx/images/Caroline.jpg"}, 
+    );
   });
